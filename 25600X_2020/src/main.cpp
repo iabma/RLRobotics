d12dec3c4953e7e1545b239696d811c533b5264e
Ian Balaguera <balagueraian@gmail.com>
2020-01-02 21:28:35 -0500

    Init

---
+++ 25600X_2020/src/main.cpp
@@ -0,0 +1,26 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       IB8                                                       */
+/*    Created:      Thu Jan 02 2020                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Drivetrain           drivetrain    2, 10, 3, 1     
+// LadderLift           motor         11              
+// ArmLift              motor         5               
+// Controller1          controller                    
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+}
